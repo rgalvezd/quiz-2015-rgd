479e2589397d774c55d2c4158bf5198f40cad6ca
Rodrigo Galvez <rodrigogalvezdiaz@outlook.com>
2015-08-02 19:56:32 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -37,8 +37,11 @@ sequelize.sync().success(function () {
     if(count === 0) { // la tabla se inicializa solo si está vacía
       Quiz.create({ pregunta: 'Capital de Italia',
                     respuesta: 'Roma'
+                  });
+      Quiz.create({ pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'
                   })
-      .success(function(){console.log('Base de datos inicializada')});
+      .then(function () { console.log('Base de datos inicializada')});
     };
   });
 });
